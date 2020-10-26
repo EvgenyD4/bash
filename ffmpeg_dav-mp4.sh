2f2fd0fef60b49cd9672dded1cc9c861755b0792
evg <rejnea@gmail.com>
2020-10-26 17:45:22 +0300

    добавил перекодирование из h264

--- ffmpeg_dav-mp4.sh
+++ ffmpeg_dav-mp4.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 PW=$(pwd)
 cd $PW
-for i in *.dav
+for i in *.h264
 do
-  ffmpeg -i "$i" -y -c copy "$i.mp4"
+  ffmpeg -i "$i" -y -c:v libx264 -b:v 1000k "$i.mp4" -hide_banner -threads 0 -strict -2 2> /dev/null
 done
+#-hide_banner -threads 0 2> /dev/null
 #-c:v libx264
-rm *.dav
-rm *.idx
+#-c copy
+rm *.dav 2> /dev/null
+rm *.idx 2> /dev/null
+rm *.h264 2> /dev/null
 cd $HOME
