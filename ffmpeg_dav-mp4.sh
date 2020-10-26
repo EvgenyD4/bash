abad5534f8e1faa957ca249e61a76ce5afa22319
evg <rejnea@gmail.com>
2020-10-26 17:50:16 +0300

    Собрать несколько видео в один файл

--- ffmpeg_dav-mp4.sh
+++ ffmpeg_dav-mp4.sh
@@ -8,7 +8,13 @@ done
 #-hide_banner -threads 0 2> /dev/null
 #-c:v libx264
 #-c copy
-rm *.dav 2> /dev/null
-rm *.idx 2> /dev/null
+
+find *.mp4 | sed 's:\ :\\\ :g'| sed 's/^/file /' > /tmp/fl-ffmpeg.txt; ffmpeg -f concat \
+ -i /tmp/fl-ffmpeg.txt -c copy ./output.mp4; rm /tmp/fl-ffmpeg.txt
+
+#rm *.dav 2> /dev/null
+#rm *.idx 2> /dev/null
 rm *.h264 2> /dev/null
+rm *.mp4 2> /dev/null
+
 cd $HOME
