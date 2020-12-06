4a569e420d9f68b6d2925810261d5a7fa912f70d
EvgenyD4 <73202653+EvgenyD4@users.noreply.github.com>
2020-12-06 17:20:00 +0300

    Update edit_dnsmasq.sh

--- edit_dnsmasq.sh
+++ edit_dnsmasq.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # закомментировать 1-ю строку, сменить интерфейс, разрешить dhcp
 sudo sed -i 's/except-interface=lo/#except-interface=wlan0/g' /etc/dnsmasq.conf 1>/dev/null
-sudo sed -i 's/no-dhcp-interface=ens2/no-dhcp-interface=wlan0/g' /etc/dnsmasq.conf 1>/dev/null
-sudo sed -i 's/no-dhcp-interface=wlan0/#no-dhcp-interface=wlan0/g' /etc/dnsmasq.conf 1>/dev/null
+sudo sed -i 's/no-dhcp-interface=ens2/no-dhcp-interface=eth0/g' /etc/dnsmasq.conf 1>/dev/null
+#sudo sed -i 's/no-dhcp-interface=wlan0/#no-dhcp-interface=wlan0/g' /etc/dnsmasq.conf 1>/dev/null
 #
-sudo sed -i 's/#interface=ens2/interface=wlan0/g' /etc/dnsmasq.conf 1>/dev/null
+sudo sed -i 's/#interface=ens2/interface=eth0/g' /etc/dnsmasq.conf 1>/dev/null
 #
-sudo sed -i '/interface=wlan0/a dhcp-range=192.168.10.3,192.168.10.20,255.255.255.0,24h' /etc/dnsmasq.conf 1>/dev/null
-C2=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-range/=' | tail -n1)
-sudo sed -i "${C2}d" /etc/dnsmasq.conf 1>/dev/null
-B2=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-range/=' | tail -n1)
-sudo sed -i "${B2}d" /etc/dnsmasq.conf 1>/dev/null
+#sudo sed -i '/interface=wlan0/a dhcp-range=192.168.10.3,192.168.10.20,255.255.255.0,24h' /etc/dnsmasq.conf 1>/dev/null
+#C2=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-range/=' | tail -n1)
+#sudo sed -i "${C2}d" /etc/dnsmasq.conf 1>/dev/null
+#B2=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-range/=' | tail -n1)
+#sudo sed -i "${B2}d" /etc/dnsmasq.conf 1>/dev/null
+##
+#sudo sed -i '/interface=wlan0/a dhcp-option=6,192.168.10.1' /etc/dnsmasq.conf 1>/dev/null
+#C3=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-option=6/=' | tail -n1)
+#sudo sed -i "${C3}d" /etc/dnsmasq.conf 1>/dev/null
+#A3=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-option=6/=' | tail -n1)
+#sudo sed -i "${A3}d" /etc/dnsmasq.conf 1>/dev/null
+##
+#sudo sed -i '/dhcp-option=6/a dhcp-option=3,192.168.10.1' /etc/dnsmasq.conf 1>/dev/null
+##C5=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-option=3/=' | tail -n 1)
+##sudo sed -i "${C5}d" /etc/dnsmasq.conf 1>/dev/null
 #
-sudo sed -i '/interface=wlan0/a dhcp-option=6,192.168.10.1' /etc/dnsmasq.conf 1>/dev/null
-C3=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-option=6/=' | tail -n1)
-sudo sed -i "${C3}d" /etc/dnsmasq.conf 1>/dev/null
-A3=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-option=6/=' | tail -n1)
-sudo sed -i "${A3}d" /etc/dnsmasq.conf 1>/dev/null
-#
-sudo sed -i '/dhcp-option=6/a dhcp-option=3,192.168.10.1' /etc/dnsmasq.conf 1>/dev/null
-#C5=$(cat /etc/dnsmasq.conf | sed -n '/dhcp-option=3/=' | tail -n 1)
-#sudo sed -i "${C5}d" /etc/dnsmasq.conf 1>/dev/null
-#
-sudo sed -i 's/server.*#53/server=172.28.253.1#53/' /etc/dnsmasq.conf 1>/dev/null
-# delete line
-C6=$(cat /etc/dnsmasq.conf | sed -n '/server.*#53/=' | tail -n 1)
-#
-sudo sed -i "${C6}d" /etc/dnsmasq.conf 1>/dev/null
+#sudo sed -i 's/server.*#53/server=172.28.253.1#53/' /etc/dnsmasq.conf 1>/dev/null
+## delete line
+#C6=$(cat /etc/dnsmasq.conf | sed -n '/server.*#53/=' | tail -n 1)
+##
+#sudo sed -i "${C6}d" /etc/dnsmasq.conf 1>/dev/null
 
 sudo /etc/init.d/dnsmasq restart
