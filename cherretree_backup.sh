f40ddfcf3d453f99942fe2679cfe9e31cc2bdc68
EvgenyD4 <73202653+EvgenyD4@users.noreply.github.com>
2020-12-06 12:26:19 +0300

    update

--- cherretree_backup.sh
+++ cherretree_backup.sh
@@ -4,4 +4,11 @@ if ping -c 1 192.168.0.100 &> /dev/null;
       find . -type f \( -iname "*.ctb" -or -iname "*.ctd" \) \
        -printf %P\\0 | rsync -vh -e ssh --files-from=- --from0 . \
        user@192.168.0.100:/Backup/user/
+ else sleep 90
+      if ping -c 1 192.168.0.100 &> /dev/null;
+       then cd /data/cherretree
+            find . -type f \( -iname "*.ctb" -or -iname "*.ctd" \) \
+             -printf %P\\0 | rsync -vh -e ssh --files-from=- --from0 . \
+             user@192.168.0.100:/Backup/user/
+ fi
 fi
