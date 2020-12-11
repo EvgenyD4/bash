e4d66042ad5bd3fbdd62b2f23a0090bb60e6f561
EvgenyD4 <73202653+EvgenyD4@users.noreply.github.com>
2020-12-12 01:35:56 +0300

    add color

--- torrent_add_link.sh
+++ torrent_add_link.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 clear
 echo "";
-echo " создать линк на видео"
-echo " для minidlna"
+echo -en "\033[32m создать линк на видео для minidlna \033[40m"
+echo -en "\E[0m";
+echo "";
 sleep 2
 INDIR='/2TB/anon/move/torrent'
+#ls -al $INDIR/ | awk '{print $9}'
 find $INDIR/ -type f -print | sed -e 's/^.......................//'
-echo " имя файла с обратным слеш, если есть пробелы"
+
+echo "";
+echo -en "\033[32m имя файла с обратным слеш, если есть пробелы \033[40m"
+echo -en "\E[0m";
+echo "";
 read N
-echo " директория назначения"
-echo "/2TB/move/en/film/thriller"
+R=$(ls /2TB/anon/move/torrent/Shrek.3.the.Third.2007.BDRip.x264.RG.tru.mkv | grep -oE "...$")
+echo "";
+echo -en "\033[32m  директория назначения \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/en/film/thriller \033[40m"
+echo -en "\E[0m";
+echo "";
 read OUTD
 if [ ! -d $OUTD ];
  then mkdir OUTD
 fi
-echo " имя конечного файла, включая исходное расширение"
+echo "";
+echo -en "\033[32m имя конечного файла, исключая расширение \033[40m"
+echo -en "\E[0m";
+echo "";
 read NOUT
-ln -n $INDIR/$N $OUTD/$NOUT
+ln -n $INDIR/$N $OUTD/$NOUT.$R
