1299153ae56f1f1c05da4af2e845113c9977c2b3
EvgenyD4 <73202653+EvgenyD4@users.noreply.github.com>
2020-12-21 04:22:07 +0300

    Update torrent_add_link.sh

--- torrent_add_link.sh
+++ torrent_add_link.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
-clear
+#clear
 echo "";
 echo -en "\033[32m создать линк на видео для minidlna \033[40m"
 echo -en "\E[0m";
 echo "";
 sleep 2
 INDIR='/2TB/anon/move/torrent'
-#ls -al $INDIR/ | awk '{print $9}'
-find $INDIR/ -type f -print | sed -e 's/^.......................//'
-
+ls -tr $INDIR
 echo "";
-echo -en "\033[32m имя файла с обратным слеш, если есть пробелы \033[40m"
+echo -en "\033[32m имя файла. исходный \033[40m"
 echo -en "\E[0m";
 echo "";
 read N
-R=$(ls $INDIR/$N | grep -oE "...$")
+R=$(ls $INDIR/"$N" | grep -oE "...$")
 echo "";
 echo -en "\033[32m  директория назначения \033[40m"
 echo "";
+echo -en "\033[32m /2TB/move/en/film/comedy \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/en/film/fantasy \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/en/film/katastrofy \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/en/film/melodrama \033[40m"
+echo "";
 echo -en "\033[32m /2TB/move/en/film/thriller \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/ru/film/comedy \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/ru/film/melodrama \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/ru/film/the_USSR \033[40m"
+echo "";
+echo -en "\033[32m /2TB/move/ru/film/thriller \033[40m"
+#echo "";
+#echo -en "\033[32m /2TB/move/ru/film/thriller \033[40m"
+
 echo -en "\E[0m";
 echo "";
 read OUTD
 if [ ! -d "$OUTD" ];
- then mkdir $OUTD
+ then mkdir -p $OUTD
 fi
 echo "";
 echo -en "\033[32m имя конечного файла, исключая расширение \033[40m"
 echo -en "\E[0m";
 echo "";
 read NOUT
-ln -n $INDIR/$N $OUTD/$NOUT.$R
+ln -n $INDIR/"$N" $OUTD/$NOUT.$R
